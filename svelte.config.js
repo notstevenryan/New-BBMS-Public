4c8400bc397b406b9a67600fa7f5e34372168204
notstevenryan <lorillasteven@yahoo.com>
2024-09-13 01:14:12 +0800

    Updated dependencies or something

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import vercel from '@sveltejs/adapter-vercel';
 
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,8 +12,12 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: vercel()
+		adapter: adapter()
 	}
 };
 
-export default config;
+export default {
+	kit: {
+	  adapter: adapter(),
+	}
+  };
